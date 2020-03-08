820e92cb1ff1a5165f9aa77dc809b6a0aced24dc
pyaiexpert <webtowardapex@outlook.com>
2020-03-09 03:28:02 +0800

    new css remove footer remove link decoration

--- frontend/src/containers/Layout.js
+++ frontend/src/containers/Layout.js
@@ -19,7 +19,7 @@ class CustomLayout extends React.Component {
     const { authenticated } = this.props;
     return (
       <div>
-        <Menu fixed="top" inverted>
+        <Menu fixed="top" className="topmenu" inverted>
           <Container>
             <Link to="/">
               <Menu.Item header>Home</Menu.Item>
@@ -40,70 +40,7 @@ class CustomLayout extends React.Component {
               )}
           </Container>
         </Menu>
-
         {this.props.children}
-
-        <Segment
-          inverted
-          vertical
-          style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-        >
-          <Container textAlign="center">
-            <Grid divided inverted stackable>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 1" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 2" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as="h4" content="Group 3" />
-                <List link inverted>
-                  <List.Item as="a">Link One</List.Item>
-                  <List.Item as="a">Link Two</List.Item>
-                  <List.Item as="a">Link Three</List.Item>
-                  <List.Item as="a">Link Four</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header inverted as="h4" content="Footer Header" />
-                <p>
-                  Extra space for a call to action inside the footer that could
-                  help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid>
-
-            <Divider inverted section />
-            <Image centered size="mini" src="/logo.png" />
-            <List horizontal inverted divided link size="small">
-              <List.Item as="a" href="#">
-                Site Map
-              </List.Item>
-              <List.Item as="a" href="#">
-                Contact Us
-              </List.Item>
-              <List.Item as="a" href="#">
-                Terms and Conditions
-              </List.Item>
-              <List.Item as="a" href="#">
-                Privacy Policy
-              </List.Item>
-            </List>
-          </Container>
-        </Segment>
       </div>
     );
   }
--- frontend/src/containers/NLPDashboard.js
+++ frontend/src/containers/NLPDashboard.js
@@ -35,64 +35,6 @@ class NlpDashboard extends Component {
         }
         return (
             <div>
-                <div style={{ height: 60 }}>
-                </div>
-                <div className="row backgray" style={{ height: 65, padding: 7.5 }}>
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8 row">
-                        <div className="col-sm-4">
-                            <div className="cusdiv"><h4 className="georgia">CLASSIFICATION</h4></div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="cusdiv"><h4 className="georgia">EXTRACTION</h4></div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="cusdiv"><h4 className="georgia">TEXT PROCESSING</h4></div>
-                        </div>
-                    </div>
-                </div>
-                <div className="row" style={{ height: 100, padding: 30 }}>
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8 row">
-                        <div className="col-sm-4">
-                            <hr className="yellow" ></hr>
-                        </div>
-                        <div className="col-sm-4">
-                            <hr className="green" ></hr>
-                        </div>
-                        <div className="col-sm-4">
-                            <hr className="blue" ></hr>
-                        </div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-2"></div>
-                    <div className="col-md-8 row">
-                        <div className="col-sm-4">
-                            <div className="customcolumn">
-                                <Button className="btn btn-primary custombtn yellow third" onClick={this.move_to_nlpprocessing} value="1"><h5 id="1">Sentiment Analysis</h5></Button>
-                                <Button className="btn btn-success custombtn yellow third" onClick={this.move_to_nlpprocessing} value="2"><h5 id="1">Topic Detection</h5></Button>
-                                <Button className="btn btn-success custombtn yellow third" onClick={this.move_to_nlpprocessing} value="3"><h5 id="1">Language Detection</h5></Button>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="customcolumn">
-                                <Button className="btn btn-success custombtn green fourth" onClick={this.move_to_nlpprocessing} value="1"><h5 id="2">Summarization</h5></Button>
-                                <Button className="btn btn-success custombtn green fourth" onClick={this.move_to_nlpprocessing} value="2"><h5 id="2">Entity Extraction</h5></Button>
-                                <Button className="btn btn-success custombtn green fourth" onClick={this.move_to_nlpprocessing} value="3"><h5 id="2">Keyword Extraction</h5></Button>
-                                <Button className="btn btn-success custombtn green fourth" onClick={this.move_to_nlpprocessing} value="4"><h5 id="2">Article Extraction</h5></Button>
-                            </div>
-                        </div>
-                        <div className="col-sm-4">
-                            <div className="customcolumn">
-                                <Button className="btn btn-success custombtn blue fourth" onClick={this.move_to_nlpprocessing} value="1"><h5 id="3">Stemmer</h5></Button>
-                                <Button className="btn btn-success custombtn blue fourth" onClick={this.move_to_nlpprocessing} value="2"><h5 id="3">Tokenizer</h5></Button>
-                                <Button className="btn btn-success custombtn blue fourth" onClick={this.move_to_nlpprocessing} value="3"><h5 id="3">Normalizer</h5></Button>
-                                <Button className="btn btn-success custombtn blue fourth" onClick={this.move_to_nlpprocessing} value="4"><h5 id="3">Similarity</h5></Button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
         );
     }
